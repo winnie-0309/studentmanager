cbac786ebb24a2179ac5d8b0232897645fd46a9a
KingmaxKong <43779578+kingmaxk@users.noreply.github.com>
2020-06-19 17:46:33 +0800

    remove useless methods

--- src/com/manager/TeacherManager.java
+++ src/com/manager/TeacherManager.java
@@ -14,30 +14,18 @@ public interface TeacherManager {
 
 	// 添加老师信息
 	public boolean addTeacher(Teacher teacher);
-	// 添加老师信息
-	public boolean addStudent(Student student);
 
 	// 删除老师信息
 	public boolean deleteTeacher(String teachId);
-	// 删除学生信息
-	public boolean deleteStudent(String studentId);
 
 	// 更新老师信息
 	public boolean updateTeacher(Teacher teacher);
-	// 更新学生信息
-	public boolean updateStudent(Student student);
 
 	// 查询一个老师的信息
 	public Teacher getTeacher(String tid);
-	// 查询所有老师的信息
-	public List<Teacher> getAllTeachers();
-	// 查询所有学生的信息
-	public List<Student> getAllStudents();
-	// 学生分页查询
-	public List<Student> getStudents(String start, String pagesize);
-	// 查询学生信息需分页
+	// 查询老师信息需分页
 	public PageModel<Teacher> getTeachers(String start, String pagesize);
-	// 查询学生信息需分页- 有查询条件 
+	// 查询老师信息需分页- 有查询条件
 	public PageModel<Teacher> queryTeachers(Map<String, String> parameters, String pageNo,
 			String pageSize);
 	
